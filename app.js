54b992a46ed3a5548e13ef958cc058a3d75d50b3
jordanrios94 <jordanrios.software@gmail.com>
2018-12-30 18:21:50 +0000

    feat: created route to create a driver

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const routes = require('./routes/routes');
+
+mongoose.connect('mongodb://localhost/joyride');
 
 const app = express();
 
+app.use(bodyParser.json());
 routes(app);
 
 module.exports = app;
